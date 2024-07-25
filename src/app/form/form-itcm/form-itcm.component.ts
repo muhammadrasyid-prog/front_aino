d81982fa72646896bc7afa728260409e03dcde3f
rasyid-epnj <muhammadrasyid712@gmail.com>
2024-07-25 10:58:14 +0700

    modal edit dan approve

--- src/app/form/form-itcm/form-itcm.component.ts
+++ src/app/form/form-itcm/form-itcm.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form-itcm',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form-itcm.component.html',
   styleUrl: './form-itcm.component.css'
 })
 export class FormItcmComponent {
 
+  isModalAddOpen = false;
+  isModalUpdateOpen = false;
+  isModalApproveOpen = false;
+
+  openModalAdd() {
+    this.isModalAddOpen = true;
+  }
+
+  closeModalAdd() {
+    this.isModalAddOpen = false;     
+  }
+
+  openModalUpdate() {
+    this.isModalUpdateOpen = true;
+  }
+
+  closeModalUpdate() {
+    this.isModalUpdateOpen = false;
+  }
+
+  openModalApprove() {
+    this.isModalApproveOpen = true;
+  }
+
+  closeModalApprove() {
+    this.isModalApproveOpen = false;
+  }
+
+  onDeleteFormITCM() {
+    Swal.fire({
+      title: "Konfirmasi",
+      text: "Anda yakin ingin menghapus Formulir ini?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Ya",
+      cancelButtonText: "Tidak",
+    }).then((result: any) => {
+      if (result.isConfirmed) {
+        // Anda bisa melakukan tindakan lain di sini jika diperlukan
+        // Misalnya, menampilkan pesan atau melakukan sesuatu setelah konfirmasi
+        console.log("Pengguna mengonfirmasi penghapusan.");
+      } else {
+        console.log("Pengguna membatalkan penghapusan.");
+      }
+    });
+  }
+  
 }
--- src/app/form/hak-akses/hak-akses.component.ts
+++ src/app/form/hak-akses/hak-akses.component.ts
@@ -1,59 +1,22 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hak-akses',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hak-akses.component.html',
   styleUrls: ['./hak-akses.component.css']
 })
-export class HakAksesComponent implements AfterViewInit, OnDestroy {
+export class HakAksesComponent {
+  
+  isModalOpen = false;
 
-  modalToggleButton: HTMLElement | null = null;
-  modalCloseButtons: NodeListOf<HTMLElement> | null = null;
-  modal: HTMLElement | null = null;
-
-  ngAfterViewInit() {
-    this.modalToggleButton = document.querySelector('[data-modal-toggle="default-modal"]');
-    this.modal = document.getElementById('default-modal');
-    this.modalCloseButtons = document.querySelectorAll('[data-modal-hide="default-modal"]');
-
-    this.addModalEventListeners();
+  openModal() {
+    this.isModalOpen = true;
   }
 
-  addModalEventListeners() {
-    if (this.modalToggleButton && this.modal) {
-      this.modalToggleButton.addEventListener('click', this.showModal);
-    }
-
-    if (this.modalCloseButtons) {
-      this.modalCloseButtons.forEach(button => {
-        button.addEventListener('click', this.hideModal);
-      });
-    }
-  }
-
-  showModal = () => {
-    if (this.modal) {
-      this.modal.classList.remove('hidden');
-      this.modal.classList.add('flex');
-    }
-  }
-
-  hideModal = () => {
-    if (this.modal) {
-      this.modal.classList.add('hidden');
-      this.modal.classList.remove('flex');
-    }
-  }
-
-  ngOnDestroy() {
-    if (this.modalToggleButton) {
-      this.modalToggleButton.removeEventListener('click', this.showModal);
-    }
-
-    if (this.modalCloseButtons) {
-      this.modalCloseButtons.forEach(button => {
-        button.removeEventListener('click', this.hideModal);
-      });
-    }
+  closeModal() {
+    this.isModalOpen = false;
   }
 }
